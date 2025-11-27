e111d226d80928314c634e6be3e3fd05a1bd4aaf
Larbibnb <Larbibnb976@gmail.com>
2025-11-27 04:12:09 +0100

    adding keys

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:weatherapp/cubites/get_weather_cubits/get_weather_cubits.dart';
 import 'package:weatherapp/cubites/get_weather_cubits/get_weather_states.dart';
 import 'package:weatherapp/cubites/llm_weather_dersciption/llm_weather_cubit.dart';
 import 'package:weatherapp/views/home_view.dart';
 
-void main() {
+void main() async {
+  await dotenv.load(fileName: ".env");
   runApp(const WeatherApp());
 }
 
--- lib/services/llm_service.dart
+++ lib/services/llm_service.dart
@@ -1,7 +1,8 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:google_generative_ai/google_generative_ai.dart';
 
 class LlmService {
-  final String _apiKey = 'AIzaSyA7DvwGAgCNxBBScMzoTgEz79x1_VReIHw';
+  final String _apiKey = dotenv.env['Gemini_Api_Key']!;
 
   late final GenerativeModel _model = GenerativeModel(
     model: 'gemini-2.0-flash',
